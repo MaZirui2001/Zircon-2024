3a98e5e53582784a646ba6aa9a66751e9d563180
mazirui2001 <454434635@qq.com>
2025-02-16 11:02:37 +0800

    Refactor Cache Test Module to Support ICache and DCache Integration
    - Add comprehensive Cache_Test_IO to include ICache and DCache interfaces
    - Integrate ICache module into the Cache_Test top-level module
    - Connect ICache pipeline, MMU, and L2 cache interfaces
    - Simplify L2 cache and AXI arbiter connections
    - Remove redundant interface connections

--- src/main/scala/Backend/Memory/Cache_Test.scala
+++ src/main/scala/Backend/Memory/Cache_Test.scala
@@ -18,27 +18,35 @@ class L2Cache_Test extends Module{
     arb.io.axi <> io.axi
 }
 
-class DCache_Test_IO extends Bundle {
+class Cache_Test_IO extends Bundle {
+    val i_pp = new I_Pipeline_IO
+    val i_mmu = new I_MMU_IO
     val d_pp = new D_Pipeline_IO
     val d_mmu = new D_MMU_IO
     val d_cmt = new D_Commit_IO
-    val ic = new L2_ICache_IO
+
     val axi = new AXI_IO
 }
 
-class DCache_Test extends Module {
-    val io = IO(new DCache_Test_IO)
+class Cache_Test extends Module {
+    val io = IO(new Cache_Test_IO)
 
+    val icache = Module(new ICache)
     val dcache = Module(new DCache)
     val l2cache = Module(new L2Cache)
     val arb = Module(new AXI_Arbiter)
+
+    icache.io.pp <> io.i_pp
+    icache.io.mmu <> io.i_mmu
+    icache.io.l2 <> l2cache.io.ic
+
     dcache.io.pp <> io.d_pp
     dcache.io.mmu <> io.d_mmu
     dcache.io.cmt <> io.d_cmt
     dcache.io.l2 <> l2cache.io.dc
+
     
     l2cache.io.mem <> arb.io.l2
     arb.io.axi <> io.axi
-    l2cache.io.ic <> io.ic
     
 }
