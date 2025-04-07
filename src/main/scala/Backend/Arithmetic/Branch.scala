14a026e4d38dc28e63de8157dd442c33a4fcf1fd
mazirui2001 <454434635@qq.com>
2025-04-07 15:40:25 +0800

    更新 Branch.scala 中的跳转逻辑，优化 real_jp 的计算方式，使用比较器模块替代原有逻辑，同时调整了跳转目标的计算方式，以提高代码的准确性和可读性。

--- src/main/scala/Backend/Arithmetic/Branch.scala
+++ src/main/scala/Backend/Arithmetic/Branch.scala
@@ -17,18 +17,23 @@ class Branch_IO extends Bundle{
 class Branch extends Module{
     val io = IO(new Branch_IO)
 
-    val real_jp = WireDefault(false.B)
-    val fail = WireDefault(Mux(io.op(4), io.pred_offset =/= Mux(real_jp, io.imm, 4.U), false.B))
-    val jump_tgt = WireDefault(io.pc + io.imm)
+    val real_jp         = WireDefault(false.B)
+    val fail            = WireDefault(Mux(io.op(4), io.pred_offset =/= Mux(real_jp, io.imm, 4.U), false.B))
+    val tgt_adder_src1  = WireDefault(io.pc)
+    val tgt_adder_src2  = WireDefault(io.imm)
+    val jump_tgt        = BLevel_PAdder32(tgt_adder_src1, tgt_adder_src2, 0.U).io.res
+    val cmp_src1        = WireDefault(io.src1)
+    val cmp_src2        = WireDefault(io.src2)  
+    val cmp_adder       = BLevel_PAdder32(cmp_src1, ~cmp_src2, 1.U)
     switch(io.op){
         is(BEQ) { real_jp := io.src1 === io.src2 }
         is(BNE) { real_jp := io.src1 =/= io.src2 }
-        is(BLT) { real_jp := io.src1.asSInt < io.src2.asSInt }
-        is(BGE) { real_jp := io.src1.asSInt >= io.src2.asSInt }
-        is(BLTU){ real_jp := io.src1 < io.src2 }
-        is(BGEU){ real_jp := io.src1 >= io.src2 }
+        is(BLT) { real_jp := cmp_src1(31) && !cmp_src2(31) || !(cmp_src1(31) ^ cmp_src2(31)) && cmp_adder.io.res(31) }
+        is(BGE) { real_jp := (!cmp_src1(31) || cmp_src2(31)) && ((cmp_src1(31) ^ cmp_src2(31)) || !cmp_adder.io.res(31)) }
+        is(BLTU){ real_jp := !cmp_adder.io.cout }
+        is(BGEU){ real_jp := cmp_adder.io.cout }
         is(JAL) { real_jp := true.B }
-        is(JALR){ real_jp := true.B; fail := io.src1 + io.imm =/= io.pred_offset; jump_tgt := io.src1 + io.imm }
+        is(JALR){ real_jp := true.B; fail := jump_tgt =/= io.pred_offset; tgt_adder_src1 := io.src1 }
     }
     io.real_jp      := real_jp
     io.pred_fail    := fail
